5c1b0f783fc94d968b98aad18abe943497b2af2e
MariInTech <107012517+MariInTech@users.noreply.github.com>
2022-06-06 21:18:50 -0400

    Add files via upload

---
+++ neko software using a for loop/src/Main.java
@@ -0,0 +1,12 @@
+//This is my first for loop application and
+//it's notifying you how many times
+//my dog barked today
+class Main {
+    //application prints Hello World//
+    public static void main(String[] args) {
+        int nekoBarked = 10;
+        for (int i = 1; i <= nekoBarked; i++) {
+            System.out.println("Neko barked");
+        }
+    }
+}
